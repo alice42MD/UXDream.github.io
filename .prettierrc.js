b7a2ad1c5bc26c94a5d42d39a617a41b3fa53706
Alice42MD <alicemartindevin@gmail.com>
2024-11-20 08:56:35 +0100

    linter config prettier eslint

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  semi: false,
+  tabWidth: 2,
+  trailingComma: "none",
+  singleQuote: false
+}
